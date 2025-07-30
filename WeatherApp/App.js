58e6f431a2f7420d533e16cf73003a94ce9e572b
Areeba Amjad <117988518+IamAreeba@users.noreply.github.com>
2025-07-30 18:00:41 +0500

    Weather app with dynamic city search
    React Native weather application that:
    - Fetches live weather data by city
    - Displays temperature, humidity, and conditions
    - Dynamically updates background images based on weather
    - Includes responsive design and search bar

---
+++ WeatherApp/App.js
@@ -0,0 +1,86 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import Weather from './components/Weather';
+import SearchBar from './components/SearchBar';
+
+
+const API_Key = "ebdfe55207c6f63e1b280ec60826e60a"
+
+export default function App() {
+
+  const [weatherData, setWeatherData] = useState(null)
+  const [loaded, setLoaded] = useState(true)  // Currently loaded thatswhy true
+  
+
+  async function fetchWeatherData(cityName){
+    setLoaded(false)
+    const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_Key}`
+    try {
+      
+      const response = await fetch(API)
+      if(response.status == 200){
+         const data = await response.json();  
+         setWeatherData(data)
+      }
+      else{
+        setWeatherData(null)
+      }
+
+      setLoaded(true)
+
+    } catch (error) {
+        console.log(error)
+    }
+  }
+ 
+  useEffect(() => {
+    fetchWeatherData('Mumbai');
+  }, []);
+
+  
+  useEffect(() => {
+    if (weatherData) {
+      console.log('Weather Data:', weatherData);
+    }
+  }, [weatherData]);
+
+  if(!loaded){
+    return(
+      <View style={styles.container}>
+          <ActivityIndicator color='gray'  size={36}   /> 
+      </View>
+    )
+  }
+
+   else if(weatherData === null) {
+        return (
+            <View style={styles.container}>
+                <SearchBar fetchWeatherData={fetchWeatherData}/>
+                <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+            </View>
+        )
+    }
+
+
+
+  return (
+    <View style={styles.container}>
+      <Weather  weatherData={weatherData}  fetchWeatherData={fetchWeatherData} />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
+});
